e60fe74e5d487f906ea8ea1ce7b38dfd115aa419
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-11 05:16:52 +0300

    Refactor the code by convert the state column to list.

--- main.py
+++ main.py
@@ -8,21 +8,19 @@ user = tr.Turtle()
 user.hideturtle()
 user.penup()
 state_count = 0
-guessed_state = []
-
 state_data = pd.read_csv("50_states.csv")
-while state_count < len(state_data.state):
+states_list = state_data.state.to_list()
+print(states_list)
+while state_count < len(state_data.state) - 1:
     user_answer = screen.textinput(title=f"You guessed {state_count}/50", prompt="Write any of the state names "
                                                                                  "correctly.").title()
-    if not state_data[state_data.state == user_answer].empty:
-        if user_answer not in guessed_state:
-            state_row = state_data[state_data.state == user_answer]
-            state_x = state_row.iloc[0, 1]
-            state_y = state_row.iloc[0, 2]
-            user.goto(state_x, state_y)
-            user.write(user_answer, move=False, align="center", font=("Verdana", 10, "normal"))
-            guessed_state.append(user_answer)
-            state_count += 1
-    
+    if user_answer in states_list:
+        state_row = state_data[state_data.state == user_answer]
+        state_x = state_row.iloc[0, 1]
+        state_y = state_row.iloc[0, 2]
+        user.goto(state_x, state_y)
+        user.write(user_answer, move=False, align="center", font=("Verdana", 10, "normal"))
+        state_count += 1
+
 
 screen.exitonclick()
