e5634dc13f25a53690502b4e57da06350f070aba
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-11 07:22:03 +0300

    Saving user progress to CSV file.

--- main.py
+++ main.py
@@ -7,18 +7,30 @@ screen.bgpic("blank_states_img.gif")
 user = tr.Turtle()
 user.hideturtle()
 user.penup()
-state_count = 0
 state_data = pd.read_csv("50_states.csv")
 states_list = state_data.state.to_list()
-print(states_list)
-while state_count < len(state_data.state) - 1:
-    user_answer = screen.textinput(title=f"You guessed {state_count}/50", prompt="Write any of the state names "
+gussed_states = []
+with open("user_progress.csv") as user_progress:
+    for line in user_progress.readlines():
+        gussed_states.append(line.strip())
+
+while len(gussed_states) < 50:
+    user_answer = screen.textinput(title=f"You guessed {len(gussed_states)}/50", prompt="Write any of the state names "
                                                                                  "correctly.").title()
     if user_answer in states_list:
         state_row = state_data[state_data.state == user_answer]
         user.goto(int(state_row.x), int(state_row.y))
         user.write(user_answer, move=False, align="center", font=("Verdana", 10, "normal"))
-        state_count += 1
+        gussed_states.append(user_answer)
+    elif user_answer.lower() == "exit":
+        with open("user_progress.csv", mode="a") as user_progress:
+            for state in gussed_states:
+                user_progress.write(f"{state}\n")
+
+        with open("user_progress.csv") as user_progress:
+            print("Ohio" in user_progress)
+
+
 
 
 screen.exitonclick()
