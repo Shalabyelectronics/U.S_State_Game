0c3db6ea5db01eaf3a5a4d10f401154bcc22eadf
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-11 07:45:19 +0300

    Trying to solve issue about check if user progress file contain data.

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import turtle as tr
+import os
 
 screen = tr.Screen()
 screen.setup(width=725, height=491)
@@ -14,10 +15,21 @@ with open("user_progress.csv") as user_progress:
     for line in user_progress.readlines():
         gussed_states.append(line.strip())
 
+
+def locate_states_progress():
+    if os.path.getsize("user_progress.csv") > 0:
+        for state_saved in gussed_states:
+            state_row_data = state_data[state_data.state == state_saved]
+            # user.goto(int(state_row_data.x), int(state_row_data.y))
+            # user.write(state_saved, move=False, align="center", font=("Verdana", 10, "normal"))
+
+
+locate_states_progress()
+
 while len(gussed_states) < 50:
     user_answer = screen.textinput(title=f"You guessed {len(gussed_states)}/50", prompt="Write any of the state names "
-                                                                                 "correctly.").title()
-    if user_answer in states_list:
+                                                                                        "correctly.").title()
+    if user_answer in states_list and user_answer not in gussed_states:
         state_row = state_data[state_data.state == user_answer]
         user.goto(int(state_row.x), int(state_row.y))
         user.write(user_answer, move=False, align="center", font=("Verdana", 10, "normal"))
@@ -26,11 +38,6 @@ while len(gussed_states) < 50:
         with open("user_progress.csv", mode="a") as user_progress:
             for state in gussed_states:
                 user_progress.write(f"{state}\n")
-
-        with open("user_progress.csv") as user_progress:
-            print("Ohio" in user_progress)
-
-
 
 
 screen.exitonclick()
