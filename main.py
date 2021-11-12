4d2517a8004ee8511d6fcd392fd27f8647064415
shalabyelectronics <mhdshalabystar@gmail.com>
2021-11-12 15:40:57 +0300

    The problem solved by checking the guessed list instead of user progress file

--- main.py
+++ main.py
@@ -1,6 +1,5 @@
 import pandas as pd
 import turtle as tr
-import os
 
 screen = tr.Screen()
 screen.setup(width=725, height=491)
@@ -10,34 +9,36 @@ user.hideturtle()
 user.penup()
 state_data = pd.read_csv("50_states.csv")
 states_list = state_data.state.to_list()
-gussed_states = []
+guessed_states = []
 with open("user_progress.csv") as user_progress:
     for line in user_progress.readlines():
-        gussed_states.append(line.strip())
+        guessed_states.append(line.strip())
 
 
 def locate_states_progress():
-    if os.path.getsize("user_progress.csv") > 0:
-        for state_saved in gussed_states:
+    if len(guessed_states) > 0:
+        for state_saved in guessed_states:
             state_row_data = state_data[state_data.state == state_saved]
-            # user.goto(int(state_row_data.x), int(state_row_data.y))
-            # user.write(state_saved, move=False, align="center", font=("Verdana", 10, "normal"))
+            user.goto(int(state_row_data.x), int(state_row_data.y))
+            user.write(state_saved, move=False, align="center", font=("Verdana", 10, "normal"))
 
 
 locate_states_progress()
 
-while len(gussed_states) < 50:
-    user_answer = screen.textinput(title=f"You guessed {len(gussed_states)}/50", prompt="Write any of the state names "
-                                                                                        "correctly.").title()
-    if user_answer in states_list and user_answer not in gussed_states:
+while len(guessed_states) < 50:
+    user_answer = screen.textinput(title=f"You guessed {len(guessed_states)}/50", prompt="Write any of the state names "
+                                                                                         "correctly.").title()
+    if user_answer in states_list and user_answer not in guessed_states:
         state_row = state_data[state_data.state == user_answer]
         user.goto(int(state_row.x), int(state_row.y))
         user.write(user_answer, move=False, align="center", font=("Verdana", 10, "normal"))
-        gussed_states.append(user_answer)
+        guessed_states.append(user_answer)
     elif user_answer.lower() == "exit":
-        with open("user_progress.csv", mode="a") as user_progress:
-            for state in gussed_states:
+        with open("user_progress.csv", mode="w") as user_progress:
+            for state in guessed_states:
                 user_progress.write(f"{state}\n")
-
+    elif user_answer.lower() == "clear":
+        with open("user_progress.csv", mode="w") as user_progress:
+            pass
 
 screen.exitonclick()
